1179087e6f861de79749759e9636ee5e08b4d72b
kane <nqkhanh160707@gmail.com>
2026-03-19 22:08:42 +0700

    my txt

---
+++ txt.pl
@@ -0,0 +1,3 @@
+teacher this is my assignment , so sorry for not knowing how to attatch the VS code to Git Hub next time i will remember to do so but
+    here is my code for the assignment 7
+    this took me almost 3h to do and 30mins to just to commit changes to github and test it for ensure it runs.
